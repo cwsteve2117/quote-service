80cc5700e9251a876d4d52583109947938e866cd
Colin Stevenson <cwsteve2117@users.noreply.github.com>
2016-09-23 17:18:51 -0400

    Update Application.java
    trigger build please!

--- src/main/java/org/springframework/nanotrader/quote/Application.java
+++ src/main/java/org/springframework/nanotrader/quote/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("trigger build for demo");
+		System.out.println("trigger build");
 		SpringApplication.run(Application.class, args);
 	}
 }
